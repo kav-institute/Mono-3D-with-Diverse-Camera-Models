e2e2b091b0bd0987838c74093d2236cafdcee380
Hannes Reichert <103506308+kav-hareichert@users.noreply.github.com>
2024-11-26 09:23:49 +0100

    Update dataloader_CARLA.py

--- src/dataset/dataloader_CARLA.py
+++ src/dataset/dataloader_CARLA.py
@@ -2,18 +2,23 @@ from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as transforms
 import torch
 import numpy as np
-from dataset.utils import build_normal_xyz
-from dataset.definitions import id_map
+try:
+    from dataset.utils import build_normal_xyz
+except:
+    from utils import build_normal_xyz
+#from dataset.definitions import id_map
 import cv2 as cv
 import glob
 
-from dataset.projection_utils import equirect2Fisheye_FOV
+try:
+    from dataset.projection_utils import equirect2Fisheye_FOV, to_deflection_coordinates
+except:
+    from projection_utils import equirect2Fisheye_FOV, to_deflection_coordinates
+
 
 def sample_from_cfg(param_list):
     param = np.random.choice(param_list)
     return param
-
-
 
 class CarlaEquirectangular(Dataset):
     def __init__(self, data_path, config):
@@ -45,12 +50,9 @@ class CarlaEquirectangular(Dataset):
         
         aspect = sample_from_cfg(self.config["ASPECT"])
         
-        frame_path, range_path, label_path = self.data_path[idx]
+        frame_path, range_path = self.data_path[idx]
 
         rgb_img = cv.imread(frame_path)
-
-        labels = cv.imread(label_path)
-        semantic_img = labels[...,2]
 
         # convert range to meter
         range_img = cv.imread(range_path, cv.IMREAD_UNCHANGED)/100.0
@@ -82,7 +84,7 @@ class CarlaEquirectangular(Dataset):
                                     return_deflection = False,
                                     return_unit_vec = True)
 
-        semantics, cam_tensor = equirect2Fisheye_FOV(semantic_img, outShape,
+        _, cam_tensor = equirect2Fisheye_FOV(range_img, outShape,
                                     f=f,
                                     w_=d,
                                     angles=[angle1, angle2 ,angle3],
@@ -117,6 +119,9 @@ class CarlaEquirectangular(Dataset):
             endcoding_img = cam_tensor
         elif self.config["SENSOR_ENCODING"] == "UnitVec":
             endcoding_img = unit_vec
+        elif self.config["SENSOR_ENCODING"] == "SHE":
+            raise ValueError
+
         elif self.config["SENSOR_ENCODING"] == "Deflection":
             endcoding_img = deflection[...,None]
         else:
@@ -127,6 +132,13 @@ class CarlaEquirectangular(Dataset):
 
         normals = build_normal_xyz(xyzi_img)
 
+        if not FLIP:
+            rgb_img = rgb_img[:,::-1,:]
+            unit_vec = unit_vec[:,::-1,:]
+            normals = normals[:,::-1,:]
+            #semantics = semantics[:,::-1]
+            range_img = range_img[:,::-1]
+            endcoding_img = endcoding_img[:,::-1,:]
 
         range_img =  torch.as_tensor(range_img[...,None].transpose(2, 0, 1).astype("float32"))
         color_img  =  torch.as_tensor(rgb_img.transpose(2, 0, 1).astype("float32"))
@@ -134,12 +146,12 @@ class CarlaEquirectangular(Dataset):
         endcoding =  torch.as_tensor(endcoding_img.transpose(2, 0, 1).astype("float32"))
         normals =  torch.as_tensor(normals.transpose(2, 0, 1).astype("float32"))
 
-        semantics =  torch.as_tensor(semantics[...,None].transpose(2, 0, 1).astype("long"))
+        #semantics =  torch.as_tensor(semantics[...,None].transpose(2, 0, 1).astype("long"))
 
-        return color_img, range_img, unit_vec, normals, semantics, endcoding
+        return color_img, range_img, unit_vec, normals, endcoding
 
 def main():
-    data_path_train = [(bin_path, bin_path.replace("rgb", "range"), bin_path.replace("rgb", "labels"))  for bin_path in glob.glob(f"/home/appuser/mnt/CARLA360/dataset/val/rgb/*.png")]
+    data_path_train = [(bin_path, bin_path.replace("rgb", "range"), bin_path.replace("rgb", "labels"))  for bin_path in glob.glob(f"/home/appuser/data/Carla/val/rgb/*.png")]
     config = {}
     config["FOV"] = list(range(20,70,1))
     config["H"] = [512]#[int(128*i) for i in [2.0, 2.5,3.0, 3.5, 4.0]]
@@ -153,9 +165,9 @@ def main():
     config["MIN_RANGE"] = 2.5
     config["SENSOR_ENCODING"] = "CameraTensor"
     depth_dataset_train = CarlaEquirectangular(data_path_train, config)
-    dataloader_train = DataLoader(depth_dataset_train, batch_size=1, shuffle=True, num_workers=1)
+    dataloader_train = DataLoader(depth_dataset_train, batch_size=1, shuffle=True)#, num_workers=1)
 
-    for batch_idx, (color_img, range_img, xyz, normals, semantics, encoding) in enumerate(dataloader_train):
+    for batch_idx, (color_img, range_img, xyz, normals, encoding) in enumerate(dataloader_train):
         rgb_img = color_img.permute(0, 2, 3, 1)[0,...].cpu().detach().numpy()
         normal_img  = normals.permute(0, 2, 3, 1)[0,...].cpu().detach().numpy()
         range_img  = range_img.permute(0, 2, 3, 1)[0,...,0].cpu().detach().numpy()
